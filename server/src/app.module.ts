6858120bd70c653a8b7ae54dd058e22b4369ef41
hamzahey <hamzakhursheed19@yahoo.com>
2025-11-28 00:48:36 +0500

    add health check endpoint

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -12,9 +12,10 @@ import { ProtectedController } from './protected/protected.controller';
 import { PrismaModule } from './prisma';
 import { AuthMiddleware } from './auth/middleware/auth.middleware';
 import { AdminModule } from './admin/admin.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, JobsModule, AdminModule],
+  imports: [PrismaModule, AuthModule, JobsModule, AdminModule, HealthModule],
   controllers: [AppController, ProtectedController],
   providers: [AppService],
 })
